3b127d3484fa687ff32b2811e169dd8e380f4eb7
eze <emollinedo@untref.edu.ar>
2020-09-24 21:47:33 -0300

    toggle class with scrolling

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
 function init(){
-    gsap.to('#intro img', {opacity: 0, scrollTrigger: {
-        trigger: '#intro',
-        start: 'top top',
-        end: 'bottom center',
-        scrub: true,
+    gsap.set("#project02", {scrollTrigger: {
+        trigger: '#project02',
+        start: 'top bottom-=10%',
+        end: 'bottom center-=10%',
+        toggleClass: 'active',
         markers: true
-    }});
+    }})
 }
 
 window.addEventListener('load', function(){
