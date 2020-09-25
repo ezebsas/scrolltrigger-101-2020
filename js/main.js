3f487c38d39e9d4e22cb472d8159f026a5fe9976
eze <emollinedo@untref.edu.ar>
2020-09-24 21:22:33 -0300

    iniciando con scroll trigger

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+gsap.registerPlugin(ScrollTrigger);
+
 function init(){
-    
+    gsap.to('#intro img', {opacity: 0, scrollTrigger: {
+        trigger: '#intro',
+        start: 'top top',
+        end: 'bottom center',
+        scrub: true,
+        markers: true
+    }});
 }
 
 window.addEventListener('load', function(){
